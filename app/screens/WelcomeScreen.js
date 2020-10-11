d69656cd21c4b74f47f6cb3564aedd7969fae414
eryson-prog <59797176+eryson-prog@users.noreply.github.com>
2020-10-11 20:09:21 +0330

    add View Image Screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 function WelcomeScreen(props) {
     return (
+
         <ImageBackground
             style={styles.background}
             source={require("../assets/background.jpg")}
-
         >
+            <View style={styles.logoContainer}>
+                <Image style={styles.logo} source={require('../assets/logo-red.png')} />
+                <Text>Sell what You Don't Need</Text>
+            </View>
             <View style={styles.loginButton}>
-
+            </View>
+            <View style={styles.registerButton}>
             </View>
         </ImageBackground>
     );
@@ -18,12 +23,28 @@ function WelcomeScreen(props) {
 
 const styles = StyleSheet.create({
     background: {
-        flex: 1
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center"
     },
     loginButton: {
         width: '100%',
         height: 70,
         backgroundColor: "#fc5c65"
+    },
+    logo: {
+        width: 100,
+        height: 100,
+    },
+    logoContainer: {
+        position: 'absolute',
+        top: 70,
+        alignItems: "center"
+    },
+    registerButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: "#4ecdc4"
     }
 })
 
